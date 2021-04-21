180f108251eb6fba08f39a92f97bd51ea382efeb
Francois Jacquier <fjacquier@gmail.com>
2021-04-21 11:54:01 +0200

    move README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Learning React / Redux 2021
+
+From Andrei / YiHua react 2021 on udemy
+Resulting URL for this project: https://crown-cc-live.herokuapp.com/
+![Image of crown-cc project](https://crown-cc-live.herokuapp.com/crown-cc-heroku.jpg)
+
+## Using
+
+- react, hooks
+- redux, thunk then sagas
+- reselect
+- redux-persist
+- styled-components
+- stripe
+- firebase
+
+## Todos
+
+- add tests
--- server.js
+++ server.js
@@ -12,15 +12,18 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression())
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "front-end/build")));
 
+  app.get("/service-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  });
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "front-end/build", "index.html"));
   });
@@ -33,9 +36,6 @@ app.listen(port, (error) => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-});
 
 app.post("/payment", (req, res) => {
 
